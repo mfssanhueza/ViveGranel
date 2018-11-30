f19eaae11c2318a07b22c1186d4f4ef981fb64a8
mfssanhueza <mfssanhueza@gmail.com>
2018-11-29 21:23:18 -0300

    activestorage

---
+++ app/models/thing.rb
@@ -0,0 +1,3 @@
+class Thing < ApplicationRecord
+  has_one_attached :image
+end
---
+++ db/migrate/20181129232652_create_things.rb
@@ -0,0 +1,11 @@
+class CreateThings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :things do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
